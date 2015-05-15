6db07f6ab8b04ff7d2b32a7232fb5bc0fc65eae4
Josh Sturgess-Meyers <gtrplyr99@gmail.com>
2015-05-15 13:15:00 -0400

    Removed Author Prompt

--- template.js
+++ template.js
@@ -1,16 +1,12 @@
 exports.description = "Sets up DRC Email Shell"
 exports.template = function(grunt, init, done) {
     init.process({}, [
-        init.prompt("name","Job Number"),
-        init.prompt("author_name")
+        init.prompt("name","Job Number")
     ], function(err, props) {
         var files = init.filesToCopy(props);
         init.copyAndProcess(files, props);
         init.writePackageJSON("package.json", {
             name: props.name,
-            author: {
-                name: props.author_name
-            },
             devDependencies: {
                 "grunt": "^0.4.5",
                 "grunt-execute": "^0.2.2",
